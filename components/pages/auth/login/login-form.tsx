5aacc86d8a150d8aa705cd5b4fedd993e2b749db
Favour Timothy <shunnode@gmail.com>
2026-04-17 21:05:56 +0100

    fixed google button alignment

--- components/pages/auth/login/login-form.tsx
+++ components/pages/auth/login/login-form.tsx
@@ -40,8 +40,8 @@ export default function LoginForm() {
   const router = useRouter();
   const openModal = useModalStore((state) => state.openModal);
   const closeModal = useModalStore((state) => state.closeModal);
-  const { mutate: googleLogin, isPending: googleSignInPending} = useGoogleLogin();
-
+  const { mutate: googleLogin, isPending: googleSignInPending } =
+    useGoogleLogin();
 
   const onSubmit = (data: LoginFormValues) => {
     const values = { email: data.email, password: data.password };
@@ -107,122 +107,125 @@ export default function LoginForm() {
       {/* Right side Form */}
       <div className="w-full lg:w-1/2 overflow-y-auto scrollbar-hide">
         <div className="min-h-full flex flex-col justify-center gap-8 md:gap-8 lg:px-10 lg:py-4">
-        {/* Logo */}
-        <div className="flex flex-col md:gap-2">
-          <div className="flex items-center gap-2 mb-8 justify-center md:justify-start">
-            <Image
-              src="/images/logo.webp"
-              alt="sit with PD logo"
-              width={30}
-              height={30}
-            />
-            <span className="font-semibold text-[14px] text-[##1E1E1E]">
-              Sit With PD
-            </span>
+          {/* Logo */}
+          <div className="flex flex-col md:gap-2">
+            <div className="flex items-center gap-2 mb-8 justify-center md:justify-start">
+              <Image
+                src="/images/logo.webp"
+                alt="sit with PD logo"
+                width={30}
+                height={30}
+              />
+              <span className="font-semibold text-[14px] text-[##1E1E1E]">
+                Sit With PD
+              </span>
+            </div>
+
+            <div>
+              <h1 className="text-[24px] font-medium md:font-bold text-brand-green mb-1 text-left">
+                Welcome Back
+              </h1>
+              <p className="text-[#475467] text-[14px] md:text-[12px]  text-left">
+                Take a moment, breathe, and continue your journey
+              </p>
+            </div>
           </div>
 
-          <div>
-            <h1 className="text-[24px] font-medium md:font-bold text-brand-green mb-1 text-left">
-              Welcome Back
-            </h1>
-            <p className="text-[#475467] text-[14px] md:text-[12px]  text-left">
-              Take a moment, breathe, and continue your journey
-            </p>
-          </div>
-        </div>
-
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-4 px-5 py-10 md:py-0 md:px-0 bg-[#FEFFFBCC] border border-[#FFFFFF5C] rounded-[5px] md:rounded-none md:border-none "
-        >
-          <div className="flex flex-col gap-5">
-            <FormFieldComp
-              control={form.control}
-              name="email"
-              label="Email address"
-              placeholder="Enter your email"
-              type="email"
-            />
-
-            <div className="space-y-1">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 px-5 py-10 md:py-0 md:px-0 bg-[#FEFFFBCC] border border-[#FFFFFF5C] rounded-[5px] md:rounded-none md:border-none "
+          >
+            <div className="flex flex-col gap-5">
               <FormFieldComp
                 control={form.control}
-                name="password"
-                label="Password"
-                placeholder="************"
-                type="password"
+                name="email"
+                label="Email address"
+                placeholder="Enter your email"
+                type="email"
               />
-              <div className="flex justify-end mt-2">
-                <Link
-                  href="/forgot-password"
-                  className="text-[12px] text-[#FDA428] font-medium hover:underline transition-colors"
-                >
-                  Forgot password?
-                </Link>
+
+              <div className="space-y-1">
+                <FormFieldComp
+                  control={form.control}
+                  name="password"
+                  label="Password"
+                  placeholder="************"
+                  type="password"
+                />
+                <div className="flex justify-end mt-2">
+                  <Link
+                    href="/forgot-password"
+                    className="text-[12px] text-[#FDA428] font-medium hover:underline transition-colors"
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* buttons  */}
-          <div className="space-y-10">
-            <Button
-              type="submit"
-              className="w-full bg-brand-green borer-[0.75px] border-brand-green text-white mt-6 rouned-[8px] md:rounded-[3.75px] text-[14px] md:text-[10.5px] font-medium  transition-colors shadow-[0px_1px_2px_0px_#1018280D] md:shadow-[0px_0.75px_1.5px_0px_#1018280D] "
-            >
-              Log in
-            </Button>
-            <div className="relative  ">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t-[0.75px] border-[#E4E4E4]"></div>
-              </div>
-              <div className="relative flex justify-center text-[12px] text-[#344054] font-medium ">
-                <span className="px-3 bg-[#FEFFFBCC]">Or</span>
-              </div>
-            </div>
-            <div className="">
-            <GoogleLogin
-              onSuccess={(credentialResponse) => {
-                const idToken = credentialResponse.credential;
-                if (!idToken) {
-                  showErrorToast("Invalid Google login credentials");
-                  return;
-                }
-
-                googleLogin({idToken}, {
-                  onSuccess: (resData) => {
-                    closeModal("loading");
-                    if (resData.data.user.role === "ADMIN") {
-                      router.replace("/admin");
-                    } else {
-                      router.replace("/user");
-                    }
-                  },
-                  onError: (error: any) => {
-                    closeModal("loading");
-                    showErrorToast(error.message);
-                  },
-                });
-              }}
-              onError={() => {
-                closeModal("loading");
-                showErrorToast("Google login failed");
-              }}
-            />
-            </div>
-          </div>
-
-          <div className="relative my-6 text-center font-medium ">
-            <p className="text-[12px] text-[#475467]">
-              Don&apos;t have an account?{" "}
-              <Link
-                href="/signup"
-                className="text-[#A8D675] hover:text-[#8cb054] font-medium transition-colors"
+            {/* buttons  */}
+            <div className="space-y-10">
+              <Button
+                type="submit"
+                className="w-full bg-brand-green borer-[0.75px] border-brand-green text-white mt-6 rouned-[8px] md:rounded-[3.75px] text-[14px] md:text-[10.5px] font-medium  transition-colors shadow-[0px_1px_2px_0px_#1018280D] md:shadow-[0px_0.75px_1.5px_0px_#1018280D] "
               >
-                Sign up
-              </Link>
-            </p>
-          </div>
-        </form>
+                Log in
+              </Button>
+              <div className="relative  ">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t-[0.75px] border-[#E4E4E4]"></div>
+                </div>
+                <div className="relative flex justify-center text-[12px] text-[#344054] font-medium ">
+                  <span className="px-3 bg-[#FEFFFBCC]">Or</span>
+                </div>
+              </div>
+              <div className="flex justify-center w-full">
+                <GoogleLogin
+                  onSuccess={(credentialResponse) => {
+                    const idToken = credentialResponse.credential;
+                    if (!idToken) {
+                      showErrorToast("Invalid Google login credentials");
+                      return;
+                    }
+
+                    googleLogin(
+                      { idToken },
+                      {
+                        onSuccess: (resData) => {
+                          closeModal("loading");
+                          if (resData.data.user.role === "ADMIN") {
+                            router.replace("/admin");
+                          } else {
+                            router.replace("/user");
+                          }
+                        },
+                        onError: (error: any) => {
+                          closeModal("loading");
+                          showErrorToast(error.message);
+                        },
+                      },
+                    );
+                  }}
+                  onError={() => {
+                    closeModal("loading");
+                    showErrorToast("Google login failed");
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="relative my-6 text-center font-medium ">
+              <p className="text-[12px] text-[#475467]">
+                Don&apos;t have an account?{" "}
+                <Link
+                  href="/signup"
+                  className="text-[#A8D675] hover:text-[#8cb054] font-medium transition-colors"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </div>
+          </form>
         </div>
       </div>
     </Card>
